5e3b8e6457da052218f850b9f8a62696e923956b
Elder <628892@bah.com>
2023-01-04 11:44:21 -0500

    add stuff

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,45 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from '@/_models';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+    
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    login(username, password) {
+
+        let user: User =  new User();
+        user.username = username;
+        user.password = password;
+
+        return this.http.post<any>(`http://localhost:8080/api/auth`, { "username" : username, "password" : password })
+            .pipe(map(resp => {
+
+                //store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('currentUser', JSON.stringify(user));
+                this.currentUserSubject.next(user);
+                return user;
+            }));
+        
+        
+    }
+
+    logout() {
+        // remove user from local storage and set current user to null
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+}
---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,44 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { User } from '@/_models';
+import { Observable } from 'rxjs';
+import { concatMap, map, mergeMap } from 'rxjs/operators';
+import { isError } from 'util';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    getAdminToken(){
+        return this.http.post<any>(`http://localhost:8080/api/auth`, { "username" : "root", "password" : "p@$$w0rd" })
+        .pipe(map(resp => { 
+            return resp["token"];
+        }));
+    }
+
+    register(user: User, adminToken:string ) {
+
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + adminToken });
+        let options = { headers: headers };
+
+        return this.http.post(`http://localhost:8080/api/users`, {
+            "firstName": user.firstName,
+            "lastName": user.lastName,
+            "username": user.username,
+            "temporaryPassword": user.password,
+            "roles": [
+              0
+            ]
+          }, options)
+        .pipe(map(resp => {
+            return resp;
+        }));
+    }
+
+}
